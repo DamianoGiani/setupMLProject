ca74f011785023f9cb3dff3439c91f5032fd854d
GiovanniBurbi <giovanniburbi10@gmail.com>
2022-10-07 16:49:58 +0100

    Add files via upload

--- ecgDatasets.py
+++ ecgDatasets.py
@@ -43,8 +43,8 @@ class ECGSequence(Sequence):
     def get_train_and_val(cls, path_to_hdf5, hdf5_dset, path_to_csv, batch_size=8, val_split=0.02):
         namefile = data_name(path_to_hdf5, val_split)
         path = './trainData/preprocessed/{}'.format(namefile)
-        std_scaler_path = './scalers/stdScaler{}%.pkl'.format(val_split)
-        minmax_scaler_path = './scalers/minMaxScaler{}%.pkl'.format(val_split)
+        std_scaler_path = './scalers/stdScaler{}%.pkl'.format(int(val_split*100))
+        minmax_scaler_path = './scalers/minMaxScaler{}%.pkl'.format(int(val_split*100))
         n_samples = len(pd.read_csv(path_to_csv, header=None))
         n_train = math.ceil(n_samples * (1 - val_split))
 
@@ -67,8 +67,8 @@ class ECGSequence(Sequence):
         self.f = h5py.File(path_to_hdf5, "r")
         self.x = self.f[hdf5_dset]
         if is_test:
-            std_scaler = load(open('./scalers/stdScaler{}%.pkl'.format(val_split), 'rb'))
-            minmax_scaler = load(open('./scalers/minMaxScaler{}%.pkl'.format(val_split), 'rb'))
+            std_scaler = load(open('./scalers/stdScaler{}%.pkl'.format(int(val_split*100)), 'rb'))
+            minmax_scaler = load(open('./scalers/minMaxScaler{}%.pkl'.format(int(val_split*100)), 'rb'))
             self.x = minmax_scaler.transform(std_scaler.transform(self.x))
         self.batch_size = batch_size
         if end_idx is None:
