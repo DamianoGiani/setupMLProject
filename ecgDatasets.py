43521f6307a84d7948f007cc3886913ce405eba5
GiovanniBurbi <giovanniburbi10@gmail.com>
2022-10-07 16:30:58 +0100

    Add files via upload

--- ecgDatasets.py
+++ ecgDatasets.py
@@ -62,7 +62,7 @@ class ECGSequence(Sequence):
         if path_to_csv is None:
             self.y = None
         else:
-            self.y = pd.read_csv(path_to_csv).values
+            self.y = pd.read_csv(path_to_csv, header=None).values
         # Get tracings
         self.f = h5py.File(path_to_hdf5, "r")
         self.x = self.f[hdf5_dset]
